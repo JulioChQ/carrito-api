526d11f11fe91d59c1e22a80fd78808e31807423
Julio Choquehuayta <juliosh10@gmail.com>
2023-06-13 14:52:24 -0500

    desplegado recurso usuario

--- src/main/java/com/websemantica/carrito/CarritoApplication.java
+++ src/main/java/com/websemantica/carrito/CarritoApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class CarritoApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(CarritoApplication.class, args);
 	}
--- src/main/java/com/websemantica/carrito/controller/UsuarioRestController.java
+++ src/main/java/com/websemantica/carrito/controller/UsuarioRestController.java
@@ -12,7 +12,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 @RestController
-@RequestMapping("/usuario")
+@RequestMapping("/api/usuario")
 public class UsuarioRestController {
     @Autowired
     private UsuarioService usuarioService;
